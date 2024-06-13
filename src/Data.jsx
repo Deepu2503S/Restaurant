0f388b49e3d09d98c59c2f493aaf8398a1592a82
Diya-16204 <151431840+Diya-16204@users.noreply.github.com>
2024-06-14 03:45:11 +0530

    Data.jsx

--- src/Data.jsx
+++ src/Data.jsx
@@ -3,7 +3,8 @@ const cardData = [
     {
       img: "https://i.pinimg.com/564x/c1/56/96/c156967c09b6734fcd9a79f5b9230dd0.jpg",
       name: "Butter Chicken",
-      price: "Rs. 250",
+      priceO: "Rs. 250",
+      priceN: " Rs. 220",
       time: "30 - 35 mins",
       description: "Authentic Indian Butter Chicken.Sauce Made of butter,spices,tomato and garlic-ginger mix with pieces of chicken."
 
@@ -11,7 +12,8 @@ const cardData = [
     {
       img: "https://i.pinimg.com/564x/10/9f/04/109f046d8e41106b9f18797d7d8600ce.jpg",
       name: "Biryani",
-      price: "Rs. 300",
+      priceO: "Rs. 300",
+      priceN: " Rs. 250",
       time: "35 - 40 mins",
       description: "Muradabadi Biriyani is typically prepared in kacchi style,in which raw chicken is cokked along with rice and spices."
 
@@ -19,15 +21,16 @@ const cardData = [
     {
       img: "https://i.pinimg.com/736x/20/5f/31/205f31d20f8de04cb4fc1bcd2be8b9a1.jpg",
       name: "Paneer Tikka",
-      price: "Rs. 180",
+      priceO: "Rs. 180",
+      priceN: " Rs. 150",
       time: "25 - 30 mins",
       description: "An indian dish where the chunks of panner are marinated in spices and rolled over skewers in a tandoor and tossed in masala."
         
     },
     {
       img: "https://i.pinimg.com/564x/6a/26/59/6a26596d569a3e2dbaf851d14b517f08.jpg",
-      name: "Masala Dosa",
-      price: "Rs. 150",
+      priceO: "Rs. 150",
+      priceN: " Rs. 120",
       time: "20 - 25 mins",
       description: "Made with rice,lenticels,potato,methi and curry leaves and served elegantly with chutney and shambhar."
 
@@ -35,7 +38,8 @@ const cardData = [
     {
       img: "https://i.pinimg.com/564x/69/b1/d6/69b1d61a28a154338e2bccd5d03ae054.jpg",
       name: "Chole Bhature",
-      price: "Rs. 120",
+      priceO: "Rs. 120",
+      priceN: " Rs. 100",
       time: "30 - 35 mins",
       description: "The most famous dish of northern india where chana is served with fluffy maida rich bhaturas."
 
@@ -43,7 +47,8 @@ const cardData = [
     {
       img: "https://i.pinimg.com/564x/4f/c7/d1/4fc7d15ed913279d26cd3f0b0f35c123.jpg",
       name: "Tandoori Chicken",
-      price: "Rs. 200",
+      priceO: "Rs. 200",
+      priceN: " Rs. 180",
       time: "40 - 45 mins",
       description: "The chicken tandoori recipe for soft,juicy kabaabs will simply melt in your mouth."
 
@@ -51,7 +56,8 @@ const cardData = [
     {
       img: "https://i.pinimg.com/564x/4a/2b/1c/4a2b1c79bbc8c35491b6815bdaef4680.jpg",
       name: "Rogan Josh",
-      price: "Rs. 280",
+      priceO: "Rs. 280",
+      priceN: " Rs. 220",
       time: "40 - 45 mins",
       description: "This aromatic curried meat dish originating from kashmir will surely serve your tastebuds in the most elegant manner."
 
@@ -59,7 +65,8 @@ const cardData = [
     {
       img: "https://i.pinimg.com/564x/c1/e5/8d/c1e58dc5b850ed2a576a325de6339719.jpg",
       name: "Palak Paneer",
-      price: "Rs. 220",
+      priceO: "Rs. 220",
+      priceN: " Rs. 200",
       time: "25 - 30 mins",
       description: "The thick paste made from pureed spinach, called palak when mixed with everyone's facourite paneer will surely serve as the most special dish of our restaurant"
 

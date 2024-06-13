8e8892644c5c602870a3c29173fdf7b8a8d665aa
Deepu2503S <168705153+Deepu2503S@users.noreply.github.com>
2024-06-14 00:17:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';  
+import { Outlet, useLocation } from 'react-router-dom';  
 import { Link } from 'react-router-dom';
+import { BsCart4 } from "react-icons/bs";
 
 function App() {
+  const location = useLocation();
+  const { username } = location.state || {};
+
   return (
     <div className="main">
+      <div className="welcome-message">
+        Welcome {username}!!
+      </div>
+      <div >
+        <Link to="/Cart" className="cart"><BsCart4 /></Link>
+      </div>
+      <Outlet />
       <nav className="navbar">
-        <div className="logo">Welcome Guest!!</div>
         <div className="div1">
           <Link to="/" className="nav-link">Home</Link>
+          <Link to="/Menu" className="nav-link">Menu</Link>
+          <Link to="/" className="nav-link">Contact</Link>
           <Link to="/Login" className="nav-link">Login</Link>
         </div>
       </nav>
-      <Outlet />
     </div>
   );
 }

fcc70fb2348f24632aba2daa3928769185e678f3
shreyasarnagar <shreyasarnagar@gmail.com>
2024-06-16 00:27:33 +0530

    Added Home Page

---
+++ src/Components/Assets/HomePage.jsx
@@ -0,0 +1,18 @@
+import { BiColor } from "react-icons/bi"
+import { IoIosArrowForward } from "react-icons/io";
+
+function openMenu(){
+    return(
+        <Menu />
+    );
+}
+export default function HomePage(){
+    return (
+        <div className="fullPage" >
+            <div className="h1">Hungry?</div>
+            <p>We've Got Your Next Favorite Meal!</p>
+            <button className="Redirect" onClick={openMenu}> <IoIosArrowForward /> </button>
+        </div>
+    )
+    }
+    

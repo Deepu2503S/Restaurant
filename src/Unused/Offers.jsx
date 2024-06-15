db3b7687178b8ac11aff172cf763a879e09a2ef3
Diya-16204 <151431840+Diya-16204@users.noreply.github.com>
2024-06-15 19:08:15 +0530

    Add files via upload

---
+++ src/Unused/Offers.jsx
@@ -0,0 +1,72 @@
+import "./Offers.css"
+
+export default function Offers(){
+return(
+    <>
+    <div className="Offers">
+    Apply These Exciting Offers On Your Order!!
+    </div>
+    
+    <div className="eachOffer">
+    <div className="Head">Eat24</div>   
+    <div  className="line1">Win excting cashbacks upto Rs.100 on orders above 500.</div>
+    <div  className="line2">Offer Validity:20 June 2024</div>
+    <div  className="line3">Offer Timing  :10AM TO 10PM</div>
+    <br></br>
+    <button className="button1">APPLY OFFER</button>
+    <br></br>
+    <br></br>
+    </div>
+    <div className="eachOffer">
+    <div className="Head">REPEAT_EXACT</div>
+    <div  className="line1">Repeat your last order and win excting cashbacks upto Rs.80 on orders above 400.</div>
+    <div  className="line2">Offer Validity:Every Sat And Sun</div>
+    <div  className="line3">Offer Timing  :24 Hours</div>
+    <br></br>
+    <button className="button1">APPLY OFFER</button>
+    <br></br>
+    <br></br>
+    </div>
+    <div className="eachOffer">
+    <div className="Head">BeIndian</div>
+    <div  className="line1">Order Indian Thali And get off upto Rs.110</div>
+    <div  className="line2">Offer Validity:18 June 2024</div>
+    <div  className="line3">Offer Timing  :10AM TO 10PM</div>
+    <br></br>
+    <button className="button1">APPLY OFFER</button>
+    <br></br>
+    <br></br>
+    </div>
+    <div className="eachOffer">
+    <div className="Head">Awesome_Tummy</div>
+    <div  className="line1">Win excting cashbacks upto Rs.300 on orders above 200.</div>
+    <div  className="line2">Offer Validity:Every Wednesday</div>
+    <div  className="line3">Offer Timing  :10AM TO 3PM</div>
+    <br></br>
+    <button className="button1">APPLY OFFER</button>
+    <br></br>
+    <br></br>
+    </div>
+    <div className="eachOffer">
+    <div className="Head">BellyZingites</div>
+    <div  className="line1">Offer valid on 10th order of our BellyZingites Family.Get free 10th Meal</div>
+    <div  className="line2">Offer Validity:Always</div>
+    <div  className="line3">Offer Timing  :24 Hours</div>
+    <br></br>
+    <button className="button1">APPLY OFFER</button>
+    <br></br>
+    <br></br>
+    </div>
+    <div className="eachOffer">
+    <div className="Head">Eat5000</div>
+    <div  className="line1">Win excting cashbacks upto Rs.500 on orders above 5000.</div>
+    <div  className="line2">Offer Validity:20 June 2024</div>
+    <div  className="line3">Offer Timing  :10AM TO 10PM</div>
+    <br></br>
+    <button className="button1">APPLY OFFER</button>
+    <br></br>
+    <br></br>
+    </div>
+    </>
+)
+}

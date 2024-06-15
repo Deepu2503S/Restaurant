2ee3cc95447e29471b473a88dfbacfce65f112d4
Diya-16204 <151431840+Diya-16204@users.noreply.github.com>
2024-06-16 02:50:33 +0530

    Contact.jsx

--- src/Components/NavBar/Contact.jsx
+++ src/Components/NavBar/Contact.jsx
@@ -1,9 +1,63 @@
+import "./Contact.css" ;
+import { FaFacebook,FaTwitter,FaInstagram } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div className="placards1">
       <div className="cards">
-        Yo
+        <div className="d">
+        <div className="name">Deepanshu Singh</div>
+        <div className="img"><img src="https://tse2.mm.bing.net/th?id=OIP.i96I-BcM8xEgPTv4N5g0ogHaFj&pid=Api&P=0&h=180"></img></div>
+        <div className="info">
+        <div className="info1">
+        <div><a className="shorten">Phone No: </a><p className="hide">7465236899</p></div>
+        </div>
+        <div className="info1">
+        <div><a className="shorten">&nbsp;&nbsp;&nbsp;Email: </a><p className="hide">deepanshu.singh@gmail.com</p></div>
+        </div>
+        <div className="socialmedia">
+        <FaFacebook/>&nbsp;
+        <FaInstagram/>&nbsp;
+        <FaTwitter/>
+        </div>
+        </div>
+        </div>
+        <div className="ss">
+        <div className="name">Shreya Sarnagar</div>
+        <div className="img"><img src="https://tse2.mm.bing.net/th?id=OIP.i96I-BcM8xEgPTv4N5g0ogHaFj&pid=Api&P=0&h=180"></img></div>
+        <div className="info">
+        <div className="info1">
+        <div ><a className="shorten">Phone No: </a><p className="hide">9823656564</p></div>
+        </div>
+        <div className="info1">
+        <div><a className="shorten">&nbsp;&nbsp;&nbsp;Email: </a><p className="hide">shreya.sarnagar@yahoo.com</p></div>
+        </div>
+
+        <div className="socialmedia">
+        <FaFacebook/>&nbsp;
+        <FaInstagram/>&nbsp;
+        <FaTwitter/>
+        </div>
+        </div>
+        </div>
+        <div className="sm">
+        <div className="name">Shambhavi Mishra</div>
+        <div className="img"><img src="https://tse2.mm.bing.net/th?id=OIP.i96I-BcM8xEgPTv4N5g0ogHaFj&pid=Api&P=0&h=180"></img></div>
+        <div className="info">
+        <div className="info1">
+        <div><a className="shorten">Phone No: </a><p className="hide">8743625367</p></div>
+        </div>
+        <div className="info1">
+        <div><a className="shorten">&nbsp;&nbsp;&nbsp;Email: </a><p className="hide">shambhavi.mishra@gmail.com</p></div>
+        </div>
+        <div className="socialmedia">
+        <FaFacebook/>&nbsp;
+        <FaInstagram/>&nbsp;
+        <FaTwitter/>
+        </div>
+        </div>
+        </div>
+
       </div>
     </div>
   )
